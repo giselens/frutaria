4b0aadf918532b662a515334a970521a6f89cb32
gisele <giselensilva08@outlook.com>
2023-09-25 23:30:08 -0300

    update

--- BancoDadosfrutaria.sql
+++ BancoDadosfrutaria.sql
@@ -1,16 +1,3 @@
-create table clientes(
-
-	id_cliente serial primary key,
-	cpf_client varchar not null,
-	nome_cli varchar not null,
-	email_cli varchar,
-	telefone_c varchar not null,
-	dt_nasc varchar, 
-	id_endereco int,
-	foreign key(id_endereco)
-	REFERENCES enderecos(id_endereco)
-);
-
 
 create table enderecos(
 	id_endereco serial primary key,
@@ -23,7 +10,19 @@ create table enderecos(
 
 );
 
-select * from clientes;
+create table clientes(
+
+	id_cliente serial primary key,
+	cpf_client varchar not null,
+	nome_cli varchar not null,
+	email_cli varchar,
+	telefone_c varchar not null,
+	dt_nasc date, 
+	id_endereco int,
+	foreign key(id_endereco)
+	REFERENCES enderecos(id_endereco)
+);
+
 
 create table funcionarios(
 	matricula serial primary key,
@@ -40,7 +39,6 @@ create table funcionarios(
 	
 );
 
-select * from funcionarios;
 
 
 create table fornecedores(
@@ -57,9 +55,6 @@ create table fornecedores(
 
 );
 
-select * from fornecedores;
-
-
 
 create table produtos(
 	id_produto serial primary key,
@@ -70,7 +65,7 @@ create table produtos(
 	
 );
 
-select * from produtos;
+
 
 
 create table pedidos(
@@ -88,9 +83,6 @@ create table pedidos(
 
 );
 
-select * from pedidos;
-
-
 
 create table fornecedores_has_produtos(
 	id_fornecedores_has_produtos serial primary key,
@@ -107,7 +99,7 @@ create table fornecedores_has_produtos(
 
 );
 
-select * from fornecedores_has_produtos ;
+
 
 
 create table pedidos_has_produtos(
@@ -124,7 +116,63 @@ create table pedidos_has_produtos(
 
 );
 
-select * from pedidos_has_produtos 
+insert into enderecos(rua,numero,bairro,cidade,uf) values ('Av. Getulio Vargas',50 , 'Centro', 'Diadema', 'SP');
+insert into enderecos(rua,numero,bairro,cidade,uf) values ('Av. das Flores',60 , 'Urauna', 'Diadema', 'SC');
+insert into enderecos(rua,numero,bairro,cidade,uf) values ('Rua das Palmeiras',100 , 'Centro', 'Joinville', 'SC');
+    
+
+insert into clientes(cpf_client,nome_cli,email_cli,telefone_c,dt_nasc,id_endereco ) values('1234567891234567','Jairo Santos', 'jairo@gmail.com','(47) 999999912','2000-08-09', 2);
+insert into clientes(cpf_client,nome_cli,email_cli,telefone_c,dt_nasc,id_endereco ) values('1234245666565555','Bia Morais Santos', 'bia@gmail.com','(47) 994459912','1990-12-09', 1);
+insert into clientes(cpf_client,nome_cli,email_cli,telefone_c,dt_nasc,id_endereco ) values('0987658657363523','Ana Maria Ribas', 'anam@gmail.com','(47) 988776655','2001-08-04', 3);
+
+
+insert into funcionarios(cpf_funcionario, nome_func, dt_nasc_func, telefone_func, email_func, id_endereco)
+values
+    ('12345678901', 'João da Silva', '1990-07-12', '47 99998060', 'joao@gmail.com', 1),
+    ('98765432109', 'Maria Souza', '2000-06-11', '47 99993455', 'maria@egmail.com', 2),
+    ('55555555555', 'José Santos', '1990-08-05', '47 988776655', 'jose@gmail.com', 3);
+  
+   
+   
+  insert into fornecedores(cpf_cnpj,nome_empresa,telefone_for,email_for,id_endereco) 
+    values ( '78.333.123/0004-90', 'Agrifrut LTDA', '47 88889999', 'agrifrut@gmail.com',1),
+    ( '30.345.553/0008-91', 'Legum LTDA', '47 33445599', 'legum@gmail.com',2),
+    ( '20.333.453/0009-08', 'Polpas LTDA', '47 45678999', 'polpas@gmail.com',3);
+   
+   insert into produtos(nome_produto,quant_produto,peso,preco)
+    VALUES ('Tomate',50,7,3.99),
+    		('Cebola',45,8,2.99),
+    		('Limao',30,4,1.99),
+  			('Colve',10,1,1.99),
+  			('Banana',30,9,2.99),
+  			('Maçã',30,9,5.99),
+  			('Uva',30,4,8.99);
+  			
+  INSERT INTO pedidos(quant_pedido, dt_pedido, id_cliente, matricula)
+    VALUES (3,'2023-08-09',1,2),
+    		(8,'2023-03-16',3,1),
+    		(15,'2023-03-09',2,3);
+    	
+INSERT INTO pedidos(quant_pedido, dt_pedido, id_cliente, matricula)
+    VALUES (5,'2023-08-12',1,1);
+
+INSERT INTO fornecedores_has_produtos(data, custo, id_fornecedor, id_produto)
+    VALUES ('2023-07-07',1000,1,2),
+    		('2023-07-12',2000,2,3),
+    		('2023-07-09',500,3,2),
+    		('2023-06-02',1500,2,1),
+    		('2023-08-12',2000,3,1);
+   
+ INSERT INTO pedidos_has_produtos(id_pedido, id_produto)
+    VALUES (1,2),
+    		(1,3), 
+    		(1,1), 
+    		(2,2), 
+    		(2,3),
+    		(3,1), 
+    		(3,2), 
+    		(3,3); 
+    		
 
 
 
